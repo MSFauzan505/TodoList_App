0e4137e256c5fe2e31b4d9d119e97e13511379b4
fauzan <msh.fauzan@gmail.com>
2025-05-24 09:31:25 +0700

    fix UI Task list

--- src/pages/TodayPage.jsx
+++ src/pages/TodayPage.jsx
@@ -19,14 +19,70 @@ const TodayPage = () => {
           variant='outlined'
           addonBefore={<PlusOutlined />}
         />
-      {/* Tasks */}
+        {/* Tasks */}
         <div className='flex flex-col border-b hover:bg-gray-100 transition-all border-gray-200 py-3 px-2'>
           <div className='flex justify-between w-full'>
-            <Checkbox className='h-full w-full'
+            <Checkbox className='h-full flex-1'
               style={{ fontSize: '1.3rem', display: 'flex', alignItems: 'center', gap: '1rem' }}>
               Task 1
             </Checkbox>
-            <RightOutlined />
+            <RightOutlined
+              className='flex justify-end cursor-pointer flex-3'
+            />
+          </div>
+
+          <div className='flex gap-5 ml-10'>
+            <span className='flex items-center text-sm gap-2'>
+              <CalendarOutlined />
+              12-12-2024
+            </span>
+            <span className='flex items-center text-sm gap-2'>
+              <span className='bg-gray-200 rounded text-sm px-2 py-0.5'>3</span>
+              <span>Subtasks</span>
+            </span>
+            <span className='flex items-center text-sm gap-2'>
+              <span className='bg-yellow-400 rounded text-sm px-2 py-2'></span>
+              <span>Personal</span>
+            </span>
+          </div>
+        </div>
+
+        <div className='flex flex-col border-b hover:bg-gray-100 transition-all border-gray-200 py-3 px-2'>
+          <div className='flex justify-between w-full'>
+            <Checkbox className='h-full flex-1'
+              style={{ fontSize: '1.3rem', display: 'flex', alignItems: 'center', gap: '1rem' }}>
+              Task 1
+            </Checkbox>
+            <RightOutlined
+              className='flex justify-end cursor-pointer flex-3'
+            />
+          </div>
+
+          <div className='flex gap-5 ml-10'>
+            <span className='flex items-center text-sm gap-2'>
+              <CalendarOutlined />
+              12-12-2024
+            </span>
+            <span className='flex items-center text-sm gap-2'>
+              <span className='bg-gray-200 rounded text-sm px-2 py-0.5'>3</span>
+              <span>Subtasks</span>
+            </span>
+            <span className='flex items-center text-sm gap-2'>
+              <span className='bg-green-400 rounded text-sm px-2 py-2'></span>
+              <span>Personal</span>
+            </span>
+          </div>
+        </div>
+
+        <div className='flex flex-col border-b hover:bg-gray-100 transition-all border-gray-200 py-3 px-2'>
+          <div className='flex justify-between w-full'>
+            <Checkbox className='h-full flex-1'
+              style={{ fontSize: '1.3rem', display: 'flex', alignItems: 'center', gap: '1rem' }}>
+              Task 1
+            </Checkbox>
+            <RightOutlined
+              className='flex justify-end cursor-pointer flex-3'
+            />
           </div>
 
           <div className='flex gap-5 ml-10'>
@@ -41,56 +97,6 @@ const TodayPage = () => {
             <span className='flex items-center text-sm gap-2'>
               <span className='bg-red-400 rounded text-sm px-2 py-2'></span>
               <span>Personal</span>
-            </span>
-          </div>
-        </div>
-
-         <div className='flex flex-col border-b hover:bg-gray-100 transition-all border-gray-200 py-3 px-2'>
-          <div className='flex justify-between w-full'>
-            <Checkbox className='h-full w-full'
-              style={{ fontSize: '1.3rem', display: 'flex', alignItems: 'center', gap: '1rem' }}>
-              Task 2
-            </Checkbox>
-            <RightOutlined />
-          </div>
-
-          <div className='flex gap-5 ml-10'>
-            <span className='flex items-center text-sm gap-2'>
-              <CalendarOutlined />
-              12-3-2025
-            </span>
-            <span className='flex items-center text-sm gap-2'>
-              <span className='bg-gray-200 rounded text-sm px-2 py-0.5'>8</span>
-              <span>Subtasks</span>
-            </span>
-            <span className='flex items-center text-sm gap-2'>
-              <span className='bg-yellow-400 rounded text-sm px-2 py-2'></span>
-              <span>Work</span>
-            </span>
-          </div>
-        </div>
-
-         <div className='flex flex-col border-b hover:bg-gray-100 transition-all border-gray-200 py-3 px-2'>
-          <div className='flex justify-between w-full'>
-            <Checkbox className='h-full w-full'
-              style={{ fontSize: '1.3rem', display: 'flex', alignItems: 'center', gap: '1rem' }}>
-              Task 3
-            </Checkbox>
-            <RightOutlined />
-          </div>
-
-          <div className='flex gap-5 ml-10'>
-            <span className='flex items-center text-sm gap-2'>
-              <CalendarOutlined />
-              12-12-2024
-            </span>
-            <span className='flex items-center text-sm gap-2'>
-              <span className='bg-gray-200 rounded text-sm px-2 py-0.5'>3</span>
-              <span>Subtasks</span>
-            </span>
-            <span className='flex items-center text-sm gap-2'>
-              <span className='bg-green-400 rounded text-sm px-2 py-2'></span>
-              <span>Family</span>
             </span>
           </div>
         </div>
