de4575e5c04bf62ec283e271ace52708a3fb9bf4
fauzan <msh.fauzan@gmail.com>
2025-06-03 10:15:12 +0700

    add folder data for dummy data

--- src/pages/CalenderPage.jsx
+++ src/pages/CalenderPage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import Calendar from '../components/Calendar';
+import todos from '../data/todos.json';
 
 const CalendarPage = () => {
-  const todos = [
-    { title: 'Belajar React', start: '2025-06-10T10:00:00', end: '2025-06-10T12:00:00' },
-  ];
 
   return (
     <div className='flex flex-col gap-8'>
