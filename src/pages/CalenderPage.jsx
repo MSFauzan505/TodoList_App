ba820408f4c4b3ef8a00f5a2e86792b9a7b0cd08
fauzan <msh.fauzan@gmail.com>
2025-06-04 10:25:57 +0700

    add heading on CalendarPage

--- src/pages/CalenderPage.jsx
+++ src/pages/CalenderPage.jsx
@@ -6,6 +6,9 @@ const CalendarPage = () => {
 
   return (
     <div className='flex flex-col gap-8'>
+      <div className='flex items-center gap-10'>
+        <h1 className='text-5xl font-bold'>Calendar</h1>
+      </div>
       <Calendar todos={todos} />
     </div>
   );
