6bdb78e748473ad71cf2726e655310bc6251a4cf
fauzan <msh.fauzan@gmail.com>
2025-06-05 10:32:28 +0700

    refactor loginpage with antd component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,34 +1,55 @@
-
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons'
 import AuthLayout from '../Layout/AuthLayout'
-import { Button, Input } from 'antd'
+import { Button, Input, Form } from 'antd'
+import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values)
+    // Kirim ke Supabase atau API login kamu di sini
+  }
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo)
+  }
 
   return (
     <AuthLayout>
       <div className='flex-1 flex justify-center items-center'>
-        <div className='flex flex-col gap-5  p-3 w-[600px] min-h-64'>
+        <div className='flex flex-col gap-5 p-3 w-[600px] min-h-64'>
           <h1 className='text-5xl font-semibold'>Sign in</h1>
 
-          <Input
-            size='large'
-            placeholder='Example@gmail.com'
-          />
-
-          <Input.Password
-            size='large'
-            placeholder='Password'
-            iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-          />
-
-          <Button
-            size='large'
-            variant='solid'
-            color='blue'
+          <Form
+            layout='vertical'
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
           >
-            Sign in
-          </Button>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: 'Please input your email!' }]}
+            >
+              <Input size='large' placeholder='example@gmail.com' />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password
+                size='large'
+                placeholder='Password'
+                iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type='primary' htmlType='submit' size='large' block>
+                Sign in
+              </Button>
+            </Form.Item>
+          </Form>
 
           <div className='flex gap-3 items-center'>
             <span className='bg-gray-200 h-0.5 rounded flex-1'></span>
@@ -36,28 +57,20 @@ const LoginPage = () => {
             <span className='bg-gray-200 h-0.5 rounded flex-1'></span>
           </div>
 
-          <div className='flex gap-5 justify-center '>
-            <Button
-            className='w-full'
-              size='large'
-              variant='outlined'
-              color='red'
-            >
+          <div className='flex gap-5 justify-center'>
+            <Button size='large' block danger>
               Sign in with Google
             </Button>
-
-            <Button
-            className='w-full'
-              size='large'
-              variant='outlined'
-            >
+            <Button size='large' block>
               Sign in with GitHub
             </Button>
           </div>
 
           <div className='flex justify-center gap-2 items-center'>
             <span>Don't have an account?</span>
-            <span className='font-semibold cursor-pointer'>Sign up</span>
+            <Link to='/register' className='font-semibold cursor-pointer'>
+              Sign up
+            </Link>
           </div>
         </div>
       </div>
