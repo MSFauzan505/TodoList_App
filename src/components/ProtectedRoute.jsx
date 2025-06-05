2bcd3e9cc80359a7ef8a80073c7d24aac6625131
fauzan <msh.fauzan@gmail.com>
2025-06-05 09:05:51 +0700

    fix logic ProtectedRoute

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
 const ProtectedRoute = ({children}) => {
     const isLogged = false
 
-    if(isLogged){
-        return <h1>Anda belum login</h1>
+    if(!isLogged){
+        return <Navigate to={'/login'} replace/>
     }
 
     return children
