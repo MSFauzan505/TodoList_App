c7ad17d62d4d0d665280acdb2e3091609fb00c4b
fauzan <msh.fauzan@gmail.com>
2025-06-09 10:00:36 +0700

    responsive UI mobile

--- src/pages/TodayPage.jsx
+++ src/pages/TodayPage.jsx
@@ -6,8 +6,8 @@ const TodayPage = () => {
     <div className='flex flex-col gap-8 '>
       {/* Title page */}
       <div className='flex items-center gap-10'>
-        <h1 className='text-5xl font-bold'>Today</h1>
-        <span className='border border-gray-200 p-3 rounded text-5xl'>12</span>
+        <h1 className='text-4xl md:text-5xl font-bold'>Today</h1>
+        <span className='border border-gray-200 p-3 rounded text-3xl md:text-5xl'>12</span>
       </div>
 
       {/* Input Task */}
