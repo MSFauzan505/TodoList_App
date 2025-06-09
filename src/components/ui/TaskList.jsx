091772a1d168bc91e2adad7fd56b73e7f022b60e
fauzan <msh.fauzan@gmail.com>
2025-06-09 10:33:37 +0700

    fix ui

--- src/components/ui/TaskList.jsx
+++ src/components/ui/TaskList.jsx
@@ -23,9 +23,9 @@ const TaskList = () => {
       <div className="overflow-y-auto flex-1 pr-1">
         {/* Tasks */}
         <div className="flex flex-col border-b hover:bg-gray-100 transition-all border-gray-200 py-3 px-2">
-          <div className="flex justify-between w-full">
+          <div className="flex justify-between w-full ">
             <Checkbox
-              className="h-full flex-1"
+              className="h-full flex-4 w-full "
               style={{
                 fontSize: '1.3rem',
                 display: 'flex',
