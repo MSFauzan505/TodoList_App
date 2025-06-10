05a164ef4b40435d165867d02adc8dc2822ec8f6
fauzan <msh.fauzan@gmail.com>
2025-06-10 08:39:14 +0700

    add crud function for tasks

---
+++ src/services/todoService.js
@@ -0,0 +1,52 @@
+import { supabase } from "../lib/supabaseClient"
+
+// create task
+export const createTodo = async (todo)=>{
+    const {data, error} = await supabase
+    .from('tasks')
+    .insert([todo])
+
+    return {data, error}
+}
+
+// delete task
+export const deleteTodo = async (id)=>{
+    const {data, error} = await supabase
+    .from('tasks')
+    .delete()
+    .eq('id', id)
+
+    return {data, error}
+}
+
+// get all taks
+export const getTodo = async ()=>{
+    const { data, error } = await supabase
+    .from('tasks')
+    .select()
+    .order('create_at', {ascending: false})
+
+    return {data, error}
+}
+
+// get task by id
+export const getTodoById = async (id)=>{
+    const {data, error} = await supabase
+    .from('tasks')
+    .select()
+    .eq('id', id)
+    .single()
+
+    return {data ,error}
+}
+
+// update task
+export const updateTodo = async (id, updateField)=> {
+    const {data, error} = await supabase
+    .from('tasks')
+    .update(updateField)
+    .eq('id',id)
+    
+    return {data, error}
+}
+
