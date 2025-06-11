4500e0eb47182cd3d5d152b8efc2281b54f66046
fauzan <msh.fauzan@gmail.com>
2025-06-11 13:43:21 +0700

    add function for fetch lists

---
+++ src/services/listsService.js
@@ -0,0 +1,30 @@
+import { supabase } from "../lib/supabaseClient"
+
+// fetch all lists
+export const getLists = async ()=>{
+    const {data, error} = await supabase
+    .from('lists')
+    .select('*')
+
+    return {data, error}
+}
+
+// add new list
+export const createList = async (newList)=>{
+    const {data, error} = await supabase
+    .from('lists')
+    .insert([newList])
+
+    return {data, error}
+}
+
+// delete list
+export const deleteList = async (id)=>{
+    const {data, error} = await supabase
+    .from('lists')
+    .delete()
+    .eq('id', id)
+
+    return {data, error}
+}
+
